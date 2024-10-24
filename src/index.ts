a0e48f4083e427de8c64eb5cf242647efea069f8
oscar sanchez <00oscarsanchez00@gmail.com>
2024-10-24 16:04:14 -0500

    removed some comments

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-const beachFlagImg = document.createElement('img');
-beachFlagImg.src =
-  'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
-
 import { User } from './User';
 import { Company } from './Company';
 import { customMap } from './Map';
 
-// what I wrote// I'm assuming that title and position will come from the faker library
+// custom marker image on map
+const beachFlagImg = document.createElement('img');
+beachFlagImg.src =
+  'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
 
 const user = new User();
 const company = new Company();
+
 const userMarkInfo = {
   name: user.name,
   location: user.location,
@@ -22,8 +22,10 @@ const companyMarkInfo = {
   pin: null,
   email: null,
 };
+
+// create map
 const map = new customMap('map');
 
-// makes it less likely that someone adding a user or company or park or whaterver will mess things up
+// create a couple of markers for testing
 const userMarker = map.createMarker(userMarkInfo);
 const companyMarker = map.createMarker(companyMarkInfo);
